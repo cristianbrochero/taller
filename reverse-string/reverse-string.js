be5f65c67a8ae430eedc0e07b1d799c21e995a8e
Cristian Brochero <cistianbrocheropacheco@gmail.com>
2021-05-26 12:25:17 -0500

    crorreccion- prueba

--- reverse-string/reverse-string.js
+++ reverse-string/reverse-string.js
@@ -1,11 +1,5 @@
 
 
-export const reverseString = () => {
-  frase = frase.split(/[^a-z']+/i);
-  let acronym = '';
-
-  for (let i = 0; i < frase.length; i++ ){
-    acronym += frase[i][0].toUpperCase();
-  }
-  return acronym
+export const reverseString = (strin) => {
+  return strin.split('').reverse().join('');
 };
